99fe82da15fa724800d40efd44567c44e592113b
Sacha Duvivier <sduvivier@walibuy.com>
2024-10-21 09:51:40 +0200

    fc Maxime Masson/Sacha Duvivier

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+<header className="bg-gray-800 p-4">
+    <nav className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+            E-Commerce
+        </div>
+        <ul className="flex space-x-4">
+            <li><a href="#" className="text-white hover:text-gray-400">Home</a></li>
+            <li><a href="#" className="text-white hover:text-gray-400">Shop</a></li>
+            <li><a href="#" className="text-white hover:text-gray-400">About</a></li>
+            <li><a href="#" className="text-white hover:text-gray-400">Contact</a></li>
+
+            <li className="text-white">Panier</li>
+        </ul>
+    </nav>
+</header>
+    );
+}
+
+export default Header;
