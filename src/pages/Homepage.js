e2cc341e06b70236e3fc256b7eceedf67b8fdd67
Sacha Duvivier <sduvivier@walibuy.com>
2024-10-21 10:30:22 +0200

    adding routes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,8 +3,8 @@ import { useAtom } from 'jotai';
 import { useEffect } from 'react';
 import { productsAtom, addToCartAtom } from '../store';
 import {useFetch} from "../hooks/useFetch";
-
-
+import Image from '../icone.webp';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     const { data: products, loading, error } = useFetch('/products');
@@ -20,6 +20,21 @@ const Homepage = () => {
     return (
         <Layout>
             <h1>Homepage</h1>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            <ul className="grid grid-cols-4 gap-3">
+                {products && products.map((product) => (
+                    <Link to={`/product/${product.id}`}>
+                        <li key={product.id} className="p-2">
+                            <img src={Image} alt="icone" />
+                            <h2>{product.name}</h2>
+                            <p>{product.description}</p>
+                            <p>${product.price}</p>
+                            <button onClick={() => addToCart(product)}>Add to Cart</button>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
         </Layout>
     )
 }
